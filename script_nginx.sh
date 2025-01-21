74eba4b9db5ec2b2bcb53f174225cd9359ee53ac
Maria <77062061+mariakarolina@users.noreply.github.com>
2025-01-21 12:57:41 -0300

    Update script_nginx.sh

--- script_nginx.sh
+++ script_nginx.sh
@@ -2,7 +2,7 @@
 echo "Script iniciado"
 
 # Diretório para salvar os logs
-logs="/home/ubuntu/project_files/logs"
+logs="/home/ubuntu/projeto/logs"
 
 # Verificar o status do serviço
 STATUS=$(systemctl is-active nginx)
@@ -13,7 +13,7 @@ if [ "$STATUS" = "active" ]; then
   echo "$TIMESTAMP - Nginx: ONLINE" >> $logs/online.log
   echo "Nginx está ONLINE"
 else
-  echo "$TIMESTAMP - Nginx: OFFLINE" >>$logs/offline.los
+  echo "$TIMESTAMP - Nginx: OFFLINE" >>$logs/offline.log
   echo "Nginx está OFFLINE"
 fi
 
